be2624d65c0713db43f1012dee5c9f330f0b30a6
teffalump <siv@riseup.net>
2013-01-11 20:30:04 -0800

    small stuff

--- cipherQuery.py
+++ cipherQuery.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python
-#
-#
-#
-#
-#
-#
 import sys,ssl,socket
 
-#ciphers
-context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
-context.set_ciphers("ALL:NULL")
+def website_cipher(url):
+    with socket.socket() as sock:
+        con = context.wrap_socket(sock)
+        con.settimeout(5)
+        try:
+            con.connect((url,PORT))
+            cipher=con.cipher()
+            return (url,cipher)
+        except: return (url, False)
 
 #port for https
-port = 443
-
-#websites list
-f = sys.argv[1]
+PORT = 443
 
 #read website list into list
+f = sys.argv[1]
 with open(f,'r') as a:
-    websites = a.read().split('\n')
+    WEBSITES = a.read().split()
+
+#cipher string
+if len(sys.argv) != 3: 
+    CIPHER_STRING = "DEFAULT"
+else:
+    CIPHER_STRING = sys.argv[2]
+
+#ssl context
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+context.set_ciphers(CIPHER_STRING)
 
 #read ciphers into list
-for site in websites:
-    with socket.socket() as sock:
-        try:
-            con = context.wrap_socket(sock)
-            con.settimeout(2)
-            con.connect((site,port))
-            cipher=con.cipher()
-            print(site,cipher)
-        except: pass
+for site in WEBSITES:
+    print(website_cipher(site))
